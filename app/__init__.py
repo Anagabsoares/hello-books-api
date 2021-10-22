4f7b1dab1e91f018830853b7b5a18b0e50b9c4a8
Ana Gabriele <gabbanasoares@gmail.com>
2021-10-22 10:45:44 -0700

    'Refactor creat_app'

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# set the configurations for your application
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
+
+    from .routes.py import books_bp
+    app.register_blueprint(books_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+
+books_bp = Blueprint('books',__name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+
+    pass 
+
+# getting book by id
+@books_bp.route("/<book_id>", methods=["GET"])
+def get_book_by_id(book_id):
+    pass 
